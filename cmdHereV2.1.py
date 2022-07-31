62c2b657170ab405685cddc5689bb23fafca60d9
Vinconscient <59665770+Vinconscient@users.noreply.github.com>
2022-07-31 21:20:32 +0200

    Update cmdHereV2.1.py

--- cmdHereV2.1.py
+++ cmdHereV2.1.py
@@ -1,3 +1,3 @@
 import subprocess,os
 path = 'cd ' + str(os.getcwd())
-process = subprocess.run(["start", "cmd", "/K",path,"&& type C:\\Users\\Sylvain\\style.txt"], shell=True)
+process = subprocess.run(["start", "cmd", "/K",path], shell=True)
